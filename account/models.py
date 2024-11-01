e903db3c9088d821cab034ccc80606bcac6d5c19
y-nagayoshi <wakasagi135@gmail.com>
2024-11-01 20:31:29 +0900

    ユーザー編集機能実装

--- account/models.py
+++ account/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    date_of_birth = models.DateTimeField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(
         upload_to='users/%Y/%m/%d/',
         blank=True
